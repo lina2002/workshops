282f541afcd29b7bff847646e98583ef15a78c09
Agnieszka Paszek <lina2002@go2.pl>
2015-04-22 21:41:39 +0200

    Product validations and averagerating added

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,15 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :reviews
+
+  validates_presence_of :title, :description, :price
+  validates_format_of :price, with: /\A\d+(\.\d{1,2})?\Z/
+
+  def average_rating
+    sum = 0
+    reviews.each do |rewiew|
+      sum += rewiew.rating.to_f
+    end
+    avg = sum / reviews.count.to_f
+  end
 end
